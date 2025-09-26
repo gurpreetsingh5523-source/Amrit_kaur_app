fd856b0dd355686663348f79df8ae4bc4f993c26
gurpreetsingh5523-source <gurpreetsingh5523@gmail.com>
2025-09-26 10:06:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class AmritKaurApp(App):
+    def build(self):
+        return Label(text="🌸 Satnam Ji 🌸 I am Amrit Kaur")
+
+if __name__ == "__main__":
+    AmritKaurApp().run()
